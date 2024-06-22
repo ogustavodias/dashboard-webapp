b65d72b06fef36764055df1bdb1507973cbff0b2
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2024-06-22 13:10:18 -0300

    test: to deploy in vercel

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Header from "./components/Header";
 
 // Routes
 import { RouterProvider } from "react-router-dom";
-import useRouter from "./hooks/useRouter";
+import useRouter from "./hooks/useRouter.tsx";
 
 // Global Context provider
 import { GlobalContextProvider } from "./context/global";
